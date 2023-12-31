f7764db479d8d9d53496f763f44909dd7395873f
Jan Sirui Chen <jan.jc.chen@gmail.com>
2023-12-31 12:12:06 +0100

    created key and traps class

---
+++ core/src/de/tum/cit/ase/maze/objects/still/Key.java
@@ -0,0 +1,26 @@
+package de.tum.cit.ase.maze.objects.still;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import de.tum.cit.ase.maze.objects.GameElement;
+
+import java.util.Vector;
+
+public class Key extends GameElement {
+    private Vector2 position;
+    @Override
+    public void render(SpriteBatch spriteBatch) {
+
+    }
+
+    @Override
+    public void update(float deltaTime) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ core/src/de/tum/cit/ase/maze/objects/still/Traps.java
@@ -0,0 +1,23 @@
+package de.tum.cit.ase.maze.objects.still;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import de.tum.cit.ase.maze.objects.GameElement;
+
+public class Traps extends GameElement {
+    private Vector2 position;
+    @Override
+    public void render(SpriteBatch spriteBatch) {
+
+    }
+
+    @Override
+    public void update(float deltaTime) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
