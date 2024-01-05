4a33285d1ffa69d3c8ea39606bb8d508a5751745
Jan Sirui Chen <jan.jc.chen@gmail.com>
2024-01-05 18:28:27 +0100

    implemented MapLoadingException

--- core/src/de/tum/cit/ase/maze/utils/MapLoader.java
+++ core/src/de/tum/cit/ase/maze/utils/MapLoader.java
@@ -2,22 +2,23 @@ package de.tum.cit.ase.maze.utils;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.math.Vector2;
+import de.tum.cit.ase.maze.objects.ObjectType;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 public final class MapLoader {
-    private static Properties map;
+    private static Map<ObjectType, List<Vector2>> map;
     public static void loadMapFile(FileHandle fileHandle) {
-        map = new Properties();
+        map = new HashMap<>();
 
         try {
-            if (fileHandle.exists()) {
-                map.load(fileHandle.read()); // Load the properties from the file
-            } else {
-                Gdx.app.error("PropertiesLoader", "File not found: " + filename);
-            }
-        } catch (IOException e) {
+
+        } catch (IndexOutOfBoundsException e) {
             Gdx.app.error("PropertiesLoader", "Error reading file: " + filename);
             e.printStackTrace();
         }
