0b6f214b50702812ac274a65ca0f55da02780724
Jan Sirui Chen <jan.jc.chen@gmail.com>
2024-01-05 22:45:37 +0100

    implementing PauseScreen

---
+++ core/src/de/tum/cit/ase/maze/utils/exceptions/MapLoadingException.java
@@ -0,0 +1,11 @@
+package de.tum.cit.ase.maze.utils.exceptions;
+
+public class MapLoadingException extends RuntimeException {
+    public MapLoadingException() {
+        super();
+    }
+
+    public MapLoadingException(String message) {
+        super(message);
+    }
+}
