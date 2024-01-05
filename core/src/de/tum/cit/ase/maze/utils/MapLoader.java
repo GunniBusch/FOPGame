dc2f75c45e14c47c3e7ce9a8230104f30d8aa2e4
Jan Sirui Chen <jan.jc.chen@gmail.com>
2024-01-05 19:16:36 +0100

    implemented ObjectTypeException

--- core/src/de/tum/cit/ase/maze/utils/MapLoader.java
+++ core/src/de/tum/cit/ase/maze/utils/MapLoader.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.math.Vector2;
 import de.tum.cit.ase.maze.objects.ObjectType;
+import de.tum.cit.ase.maze.utils.exceptions.MapLoadingException;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -13,14 +14,14 @@ import java.util.Properties;
 
 public final class MapLoader {
     private static Map<ObjectType, List<Vector2>> map;
+
     public static void loadMapFile(FileHandle fileHandle) {
         map = new HashMap<>();
 
         try {
 
         } catch (IndexOutOfBoundsException e) {
-            Gdx.app.error("PropertiesLoader", "Error reading file: " + filename);
-            e.printStackTrace();
+            throw new MapLoadingException("Can't load map");
         }
 
     }
---
+++ core/src/de/tum/cit/ase/maze/utils/exceptions/ObjectTypeException.java
@@ -0,0 +1,10 @@
+package de.tum.cit.ase.maze.utils.exceptions;
+
+public class ObjectTypeException extends RuntimeException{
+    public ObjectTypeException() {
+    }
+
+    public ObjectTypeException(String message) {
+        super(message);
+    }
+}
