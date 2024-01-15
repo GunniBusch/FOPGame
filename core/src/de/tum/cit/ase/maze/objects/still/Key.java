3d3e72b85d23c02c89af805939dc1a15809830dc
Jan Sirui Chen <jan.jc.chen@gmail.com>
2024-01-15 11:00:09 +0100

    added outline of ingame asset key

--- core/src/de/tum/cit/ase/maze/objects/still/Key.java
+++ core/src/de/tum/cit/ase/maze/objects/still/Key.java
@@ -2,13 +2,27 @@ package de.tum.cit.ase.maze.objects.still;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.World;
 import de.tum.cit.ase.maze.objects.GameElement;
+import de.tum.cit.ase.maze.screens.GameScreen;
 
 public class Key extends GameElement {
     private Vector2 position;
     private boolean isCollected;
 
-    
+    public Key(World world, Vector2 position, GameScreen game) {
+        BodyDef def = new BodyDef();
+        def.type = BodyDef.BodyType.StaticBody;
+        def.fixedRotation = true;
+        body = world.createBody(def);
+        body.setAwake(true);
+        createBody(position);
+    }
+
+    private void createBody(Vector2 position) {
+
+    }
     @Override
     public void render(SpriteBatch spriteBatch) {
 
