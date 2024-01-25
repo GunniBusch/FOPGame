55246a17c508269613b9b6252c3bb721074e70c7
Leon Adomaitis <leon@adomaitis.de>
2024-01-25 23:23:27 +0100

    urgent fix file chooser

--- desktop/src/de/tum/cit/ase/maze/DesktopLauncher.java
+++ desktop/src/de/tum/cit/ase/maze/DesktopLauncher.java
@@ -3,7 +3,7 @@ package de.tum.cit.ase.maze;
 import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import games.spooky.gdx.nativefilechooser.desktop.DesktopFileChooser;
+import de.tum.cit.ase.maze.utils.FixedDesktopFileChooser;
 
 /**
  * The DesktopLauncher class is the entry point for the desktop version of the Maze Runner game.
@@ -30,6 +30,6 @@ public class DesktopLauncher {
         config.useVsync(true); // Enable vertical sync
         config.setForegroundFPS(60); // Set the foreground frames per second
         // Launch the game
-        new Lwjgl3Application(new MazeRunnerGame(new DesktopFileChooser()), config);
+        new Lwjgl3Application(new MazeRunnerGame(new FixedDesktopFileChooser()), config);
     }
 }
