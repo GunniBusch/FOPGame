6b2f7bc63dc52e53a81677c934bbcb226dd598f9
Jan Sirui Chen <jan.jc.chen@gmail.com>
2024-01-26 06:36:49 +0100

    Trap2

---
+++ core/src/de/tum/cit/ase/maze/objects/still/Trap2.java
@@ -0,0 +1,48 @@
+package de.tum.cit.ase.maze.objects.still;
+
+import box2dLight.RayHandler;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import de.tum.cit.ase.maze.objects.dynamic.Player;
+import de.tum.cit.ase.maze.objects.still.collectable.TimedCollectable;
+
+import static de.tum.cit.ase.maze.utils.CONSTANTS.PPM;
+
+public class Trap2 extends TimedCollectable {
+    private final TextureRegion textureRegion;
+
+    public Trap2(Vector2 position, World world, RayHandler rayHandler, TextureAtlas textureAtlas) {
+        super(position, world, rayHandler, textureAtlas);
+        texture = new Texture("trap2.png");
+        textureRegion = new TextureRegion(texture);
+        active = false;
+        duration = 10;
+    }
+
+    @Override
+    protected void apply(Player player) {
+        player.setSpeed(player.getSpeed() / 2);
+    }
+
+    @Override
+    public void restore(Player player) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch spriteBatch) {
+        spriteBatch.draw(
+                this.textureRegion,
+                this.body.getPosition().x * PPM - (16 / 2f),
+                body.getPosition().y * PPM - (16 / 2f),
+                frameWidth * ZOOM,
+                frameHeight * ZOOM
+        );
+    }
+
+
+}
